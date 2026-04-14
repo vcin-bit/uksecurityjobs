478f29ff0a91539031f66f5037c8959b0b8855df
UKSecurityJobs Build <api@uksecurityjobs.co.uk>
2026-04-14 12:54:07 +0000

    Update email sender to admin@uksecurityjobs.co.uk

--- api/src/lib/email.js
+++ api/src/lib/email.js
@@ -3,7 +3,7 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const FROM = {
-  email: 'noreply@uksecurityjobs.co.uk',
+  email: 'admin@uksecurityjobs.co.uk',
   name: 'UKSecurityJobs'
 };
 
